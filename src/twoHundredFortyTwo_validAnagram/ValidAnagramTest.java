cec6c6f3a5d8f26bab035f5282418d2090aba2e3
Gian Paolo Buffo <gianpaolo.buffo@tado.com>
2023-10-30 15:42:20 +0100

    Update leetcode 242

---
+++ src/twoHundredFortyTwo_validAnagram/ValidAnagramTest.java
@@ -0,0 +1,32 @@
+package twoHundredFortyTwo_validAnagram;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidAnagramTest {
+
+    @Test
+    void testCaseOne() {
+        // given
+        var s = "anagram";
+        var t = "nagaram";
+
+        // when
+        var result = new ValidAnagram().isAnagram(s, t);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testCaseTwo() {
+        // given
+        var s = "rat";
+        var t = "car";
+
+        // when
+        var result = new ValidAnagram().isAnagram(s, t);
+
+        assertFalse(result);
+    }
+}
