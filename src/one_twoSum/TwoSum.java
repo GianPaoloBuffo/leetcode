b7fbc03afd8ea558e1d49233eec01a4e72ebffb6
Gian Paolo Buffo <gianpaolo.buffo@tado.com>
2023-10-30 17:22:44 +0100

    Update leetcode 1

---
+++ src/one_twoSum/TwoSum.java
@@ -0,0 +1,53 @@
+package one_twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers
+such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+Constraints:
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+
+class TwoSum {
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[]{i, numToIndex.get(complement)};
+            }
+            numToIndex.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("There should always be a solution");
+    }
+}
